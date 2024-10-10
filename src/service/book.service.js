2623e5ef523fee521c511cd0c6346fc41b889fb4
Mohamed Abul Faiz <abulfaiz.doodleblue@gmail.com>
2024-10-10 15:36:58 +0530

    swagger output changes

--- src/service/book.service.js
+++ src/service/book.service.js
@@ -6,36 +6,32 @@ const { statusCodeEnum } = require("../helper/status.enum");
 const { errorHandler } = require("../helper/errorHandler.helper");
 
 async function createBook(insertData) {
-    await new books(insertData).save();
-    return response(
-      responseEnum.Success,
-      statusCodeEnum.HTTP_CREATED,
-      responseEnum.Created
-    );
+  await new books(insertData).save();
+  return response(
+    responseEnum.Success,
+    statusCodeEnum.HTTP_CREATED,
+    responseEnum.Created
+  );
 }
 
 async function getAll(pageNumber, pageSize, query = {}) {
-    pageNumber = Number(pageNumber) || 1;
-    pageSize = Number(pageSize) || 10;
-    const skip = (pageNumber - 1) * pageSize;
-    let searchQuery = {};
-    if (query.title) {
-      searchQuery.title = new RegExp(query.title, "i");
-    }
-    if (query.author) {
-      searchQuery.author = new RegExp(query.author, "i");
-    }
-    if (
-      query.genres &&
-      Array.isArray(query.genres) &&
-      query.genres.length > 0
-    ) {
-      searchQuery.genres = { $in: query.genres };
-    }
-    const totalCount = await books.find(searchQuery).countDocuments();
-    const data = await books.find(searchQuery).skip(skip).limit(pageSize);
-    const result = pagination(data, pageNumber, pageSize, totalCount);
-    return response(responseEnum.Success, statusCodeEnum.HTTP_OK, result);
+  pageNumber = Number(pageNumber) || 1;
+  pageSize = Number(pageSize) || 10;
+  const skip = (pageNumber - 1) * pageSize;
+  let searchQuery = {};
+  if (query.title) {
+    searchQuery.title = { $regex: query.title, $options: "i" };
+  }
+  if (query.author) {
+    searchQuery.author = { $regex: query.author, $options: "i" };
+  }
+  if (query.genres && Array.isArray(query.genres) && query.genres.length > 0) {
+    searchQuery.genres = { $in: query.genres };
+  }
+  const totalCount = await books.find(searchQuery).countDocuments();
+  const data = await books.find(searchQuery).skip(skip).limit(pageSize);
+  const result = pagination(data, pageNumber, pageSize, totalCount);
+  return response(responseEnum.Success, statusCodeEnum.HTTP_OK, result);
 }
 
 async function getById(id) {
